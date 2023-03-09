589e517bcf274d9a53c1e17d1539c3ce1d9c7154
Patrick Elsen <pelsen@xfbs.net>
2023-03-09 21:03:56 +0100

    Adds note about the application being experimental

--- src/components.rs
+++ src/components.rs
@@ -26,11 +26,12 @@ pub fn Home() -> Html {
             <SimpleNavbar />
             <Content>
                 <div style="width: 700px; margin: auto; padding-top: 20px;">
-                    <span>
-                        <h1>{ "diff.rs" }</h1>
-                    </span>
-                    <p>{ "View the differences between Rust crate versions. Enter a crate name such as "}<a href="/serde/">{"serde"}</a>{" in the search field in the top-right corner to get started." }</p>
-                    <p>{ "This is an experimental WASM-based web application written in Rust with "}<a href="https://docs.rs/yew">{"Yew"}</a>{" that uses the "}<a href="https://crates.io/">{"crates.io"}</a>{" API to fetch crate metadata, fetch and parse the crate source, unpack it in-memory and render a diff in the browser." }</p>
+                    <Callout title={"Warning"} intent={Intent::Warning}>
+                        <p>{"This application is experimental. Some crates might not load correctly due to an issue with CORS headers from crates.io."}</p>
+                    </Callout>
+                    <h1>{ "diff.rs" }</h1>
+                    <p>{ "View the differences between Rust crate versions. Enter a crate name such as "}<a href="/serde/1.0.153/1.0.153">{"serde"}</a>{" in the search field in the top-right corner to get started." }</p>
+                    <p>{ "This is a WebAssembly-based web application written in Rust with "}<a href="https://docs.rs/yew">{"Yew"}</a>{". It uses the "}<a href="https://crates.io/">{"crates.io"}</a>{" API to fetch crate metadata, downloads and parses the crate sources in-memory and renders a diff in, all in the browser." }</p>
                     <p>{"Source code for this application is available at "}<a href="https://github.com/xfbs/diff.rs">{"github.com/xfbs/diff.rs"}</a>{"."}</p>
                 </div>
             </Content>
