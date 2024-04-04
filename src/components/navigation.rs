c535421fcf54fad22b0e1a511415683b99537f3f
Patrick Elsen <pelsen@xfbs.net>
2024-04-04 17:32:16 -0400

    Fixes dark mode for navbar

--- src/components/navigation.rs
+++ src/components/navigation.rs
@@ -1,6 +1,8 @@
 use super::*;
 use crate::data::CrateResponse;
+use indexmap::IndexMap;
 use semver::Version;
+use web_sys::HtmlSelectElement;
 use yew_icons::{Icon as YewIcon, IconId};
 
 #[derive(Properties, PartialEq)]
@@ -41,7 +43,7 @@ pub struct NavbarHeadingProps {
 #[function_component]
 pub fn NavbarHeading(props: &NavbarHeadingProps) -> Html {
     html! {
-        <div class="text-xl font-bold text-nowrap">
+        <div class="text-xl font-bold text-nowrap flex flex-row items-center">
         { for props.children.iter() }
         </div>
     }
@@ -86,6 +88,47 @@ pub fn SimpleNavbar() -> Html {
 }
 
 #[derive(Properties, PartialEq)]
+pub struct SelectProps {
+    #[prop_or_default]
+    values: IndexMap<IString, IString>,
+    #[prop_or_default]
+    selected: Option<IString>,
+    #[prop_or_default]
+    onchange: Callback<IString>,
+}
+
+#[function_component]
+pub fn Select(props: &SelectProps) -> Html {
+    let onchange = {
+        let onchange = props.onchange.clone();
+        move |event: Event| {
+            let target = event.target_dyn_into::<HtmlSelectElement>().unwrap();
+            let value = target.value();
+            onchange.emit(value.into());
+        }
+    };
+    html! {
+        <select class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" {onchange}>
+        {
+            for props
+                .values
+                .iter()
+                .map(|(key, value)| {
+                    let selected = props
+                        .selected
+                        .as_ref()
+                        .map(|k| k == key)
+                        .unwrap_or(false);
+                    html! {
+                        <option {selected} value={key}>{value}</option>
+                    }
+                })
+        }
+        </select>
+    }
+}
+
+#[derive(Properties, PartialEq)]
 pub struct ComplexNavbarProps {
     pub name: String,
     pub old: Version,
@@ -96,10 +139,20 @@ pub struct ComplexNavbarProps {
 }
 
 #[function_component]
+fn SwitchIcon() -> Html {
+    html! {
+        <svg class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+            <path fill="currentColor" d="M7.72 21.78a.75.75 0 0 0 1.06-1.06L5.56 17.5h14.69a.75.75 0 0 0 0-1.5H5.56l3.22-3.22a.75.75 0 1 0-1.06-1.06l-4.5 4.5a.75.75 0 0 0 0 1.06l4.5 4.5Zm8.56-9.5a.75.75 0 1 1-1.06-1.06L18.44 8H3.75a.75.75 0 0 1 0-1.5h14.69l-3.22-3.22a.75.75 0 0 1 1.06-1.06l4.5 4.5a.75.75 0 0 1 0 1.06l-4.5 4.5Z">
+            </path>
+        </svg>
+    }
+}
+
+#[function_component]
 pub fn ComplexNavbar(props: &ComplexNavbarProps) -> Html {
     let prop_versions: Vec<_> = props.info.versions.iter().map(|v| v.num.clone()).collect();
 
-    let versions: IArray<(IString, AttrValue)> = props
+    let versions: IndexMap<IString, IString> = props
         .info
         .versions
         .iter()
@@ -112,6 +165,16 @@ pub fn ComplexNavbar(props: &ComplexNavbarProps) -> Html {
             }
         })
         .collect();
+
+    let navigator = use_navigator();
+
+    let switch = {
+        let onchange = props.onchange.clone();
+        let versions = (props.new.clone(), props.old.clone());
+        move |_| {
+            onchange.emit(versions.clone());
+        }
+    };
 
     html! {
         <Navbar>
@@ -126,12 +189,9 @@ pub fn ComplexNavbar(props: &ComplexNavbarProps) -> Html {
                     { props.name.clone() }
                 </NavbarItem>
                 <NavbarItem>
-                    <HtmlSelect<IString>
-                        minimal={true}
-                        options={versions.clone()}
-                        disabled={prop_versions.is_empty()}
-                        value={Some(props.old.to_string().into()) as Option<IString>}
-                        class="text-current dark:text-gray-200"
+                    <Select
+                        values={versions.clone()}
+                        selected={Some(props.old.to_string().into()) as Option<IString>}
                         onchange={
                             let onchange = props.onchange.clone();
                             let new = props.new.clone();
@@ -142,13 +202,15 @@ pub fn ComplexNavbar(props: &ComplexNavbarProps) -> Html {
                         }
                     />
                 </NavbarItem>
-                <NavbarItem>{ "diff" }</NavbarItem>
                 <NavbarItem>
-                    <HtmlSelect<IString>
-                        minimal={true}
-                        options={versions}
-                        disabled={prop_versions.is_empty()}
-                        value={Some(props.new.to_string().into()) as Option<IString>}
+                    <span class="cursor-pointer hover:rotate-180 transition delay-150 duration-300 ease-in-out" onclick={switch}>
+                        <SwitchIcon />
+                    </span>
+                </NavbarItem>
+                <NavbarItem>
+                    <Select
+                        values={versions}
+                        selected={Some(props.new.to_string().into()) as Option<IString>}
                         onchange={
                             let onchange = props.onchange.clone();
                             let old = props.old.clone();
--- src/components/search.rs
+++ src/components/search.rs
@@ -2,31 +2,43 @@ use super::*;
 use web_sys::HtmlInputElement;
 
 #[function_component]
+fn SearchGlass() -> Html {
+    html! {
+        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+        </svg>
+    }
+}
+
+#[function_component]
 pub fn Search() -> Html {
     let state = use_state(|| "".to_string());
     let navigator = use_navigator().unwrap();
+
+    let oninput = {
+        let state = state.clone();
+        move |event: InputEvent| {
+            state.set(event.target_unchecked_into::<HtmlInputElement>().value());
+        }
+    };
+
+    let onkeydown = {
+        let state = state.clone();
+        move |event: KeyboardEvent| {
+            if event.key() == "Enter" {
+                navigator.push(&Route::Crate {
+                    name: state.to_string(),
+                });
+            }
+        }
+    };
+
     html! {
-        <InputGroup
-            placeholder="Search crates..."
-            fill={true}
-            left_icon={Icon::Search}
-            value={state.to_string()}
-            oninput={
-                let state = state.clone();
-                move |event: InputEvent| {
-                    state.set(event.target_unchecked_into::<HtmlInputElement>().value());
-                }
-            }
-            onkeydown={
-                let state = state;
-                move |event: KeyboardEvent| {
-                    if event.key() == "Enter" {
-                        navigator.push(&Route::Crate {
-                            name: state.to_string(),
-                        });
-                    }
-                }
-            }
-        />
+        <div class="relative">
+            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                <SearchGlass />
+            </div>
+            <input type="search" id="default-search" class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search crates..." value={state.to_string()} {oninput} {onkeydown} />
+        </div>
     }
 }
