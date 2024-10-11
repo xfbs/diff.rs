a633dd25569b0ffcbbc87ea34725bf9d764bf08d
Patrick Elsen <pelsen@xfbs.net>
2024-10-11 22:14:30 +0200

    Adds documentation comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+//! # diff.rs
+//!
+//! Web application to visualize code differences between different versions of Rust crates.  Fully
+//! backend-less, works by downloading crate sources in the browser, validating the hash, unpacking
+//! it, running a diff algorithm over the files and rendering the diff. Support syntax highlighting
+//! provided by the `syntect` crate.
+
 pub mod app;
 mod cache;
 pub mod components;
