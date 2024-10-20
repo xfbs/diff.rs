4f9b256593360dd9f8853fbb971f112268503ebf
Patrick Elsen <pelsen@xfbs.net>
2024-10-20 14:59:48 +0200

    Adds file icon to diff view

--- src/components/diff_view.rs
+++ src/components/diff_view.rs
@@ -34,6 +34,16 @@ pub enum DiffStyle {
 }
 
 #[function_component]
+fn FileIcon() -> Html {
+    // from https://www.svgrepo.com/svg/491619/doc
+    html! {
+        <svg class="fill-gray-500 w-4" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M6 1C4.34314 1 3 2.34315 3 4V20C3 21.6569 4.34315 23 6 23H19C20.6569 23 22 21.6569 22 20V10C22 9.73478 21.8946 9.48043 21.7071 9.29289L13.7071 1.29292C13.6114 1.19722 13.4983 1.1229 13.3753 1.07308C13.2572 1.02527 13.1299 1 13 1H6ZM12 3H6C5.44771 3 5 3.44771 5 4V20C5 20.5523 5.44772 21 6 21H19C19.5523 21 20 20.5523 20 20V11H13C12.4477 11 12 10.5523 12 10V3ZM18.5858 9.00003L14 4.41424V9.00003H18.5858Z" />
+        </svg>
+    }
+}
+
+#[function_component]
 pub fn DiffView(props: &DiffViewProps) -> Html {
     let empty = FileDiff::default();
     let file_diff = props.diff.files.get(&props.path).unwrap_or(&empty);
@@ -92,6 +102,7 @@ pub fn DiffView(props: &DiffViewProps) -> Html {
     html! {
         <div class="diff-view">
             <div class="header">
+                <FileIcon />
                 <span class="filename">{props.path.file_name().unwrap_or("")}</span>
             </div>
             <div class="content">
